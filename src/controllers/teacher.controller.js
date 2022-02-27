9a870e1596c3f9c37cce09b15965e36d3b8486cc
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-02-28 02:10:28 +0530

    implement validations

--- src/controllers/teacher.controller.js
+++ src/controllers/teacher.controller.js
@@ -1,13 +1,31 @@
+const bcryptjs = require("bcryptjs");
 const Teacher = require("../model/TeacherModel");
+const { registerValidation } = require("../validations/teacherValidation");
 
-const addTeacher = (req,res) => {
+const addTeacher = async (req,res) => {
+
+    //validate the user input feilds
+    const {error} = registerValidation(req.body);
+    if(error) {
+        res.send({message:error['details'][0]['message']});
+    }
+
+    //to check user already exist
+    const userExist = await Teacher.findOne({email:req.body.email});
+    if(userExist) {
+        return res.status(400).send({message: "User alredy exist"})
+    }
+
+    //hash the password
+    const salt = await bcryptjs.genSalt(5);
+    const hashPassword = await bcryptjs.hash(req.body.password, salt);
 
     const teacher = new Teacher({
         firstName: req.body.firstName,
         lastName: req.body.lastName,
         NIC: req.body.NIC,
         IDNumber: req.body.IDNumber,
-        password: req.body.password,
+        password: hashPassword,
         address: req.body.address,
         phoneNumber: req.body.phoneNumber,
         email: req.body.email,
