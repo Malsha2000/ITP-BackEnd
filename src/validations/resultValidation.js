1552d2d399de294a124367810fa527da763830fb
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-03-08 20:49:07 +0530

    result validation

---
+++ src/validations/resultValidation.js
@@ -0,0 +1,18 @@
+const Joi = require("joi");
+
+const resultValidation = (data) => {
+    const schemaValidation = Joi.object({
+        examId: Joi.string().required(),
+        examName: Joi.string().required(),
+        studentName: Joi.string().required(),
+        studentId: Joi.string().required(),
+        marks: Joi.number().required(),
+        subject: Joi.string().required(),
+        grade: Joi.string().required(),
+    
+    });
+
+    return schemaValidation.validate(data);
+}
+
+module.exports.resultValidation = resultValidation;
