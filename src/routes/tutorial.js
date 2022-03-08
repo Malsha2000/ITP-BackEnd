269e4642e1f7670ddd907dcd9026f55ed4d55fa1
“[Navodhya <navodhya2011@gmail.com>
2022-03-09 00:33:22 +0530

    Implement get route

--- src/routes/tutorial.js
+++ src/routes/tutorial.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
-const { addTutorial } = require("../controllers/tutorial.controller");
+const { addTutorial, getTutorial} = require("../controllers/tutorial.controller");
 const Tutorial =require("../model/TutorialModels");
 const tutorialValidation = require("../validations/tutorialValidation");
 
 //define user routes
 router.post("/add", addTutorial);
+router.get("/all", getTutorial);
 
 module.exports = router
