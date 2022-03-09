4593ad0844fac00c2b7bef104d0c0d74c1f1cfc8
“[Navodhya <navodhya2011@gmail.com>
2022-03-09 10:40:40 +0530

    Correction implementation

--- src/routes/tutorial.js
+++ src/routes/tutorial.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const { addTutorial, getTutorial} = require("../controllers/tutorial.controller");
-const Tutorial =require("../model/TutorialModels");
-const tutorialValidation = require("../validations/tutorialValidation");
+
+
 
 //define user routes
 router.post("/add", addTutorial);
