a02b11940007c71683e17896504021dde62685cc
Udul-D <uduldulsara@gmail.com>
2022-03-13 16:22:57 +0530

    Implement routes

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
-const {addAdmin, getAdmins} = require('../controllers/admin.controller');
+const {addAdmin, getAdmins, updateAdmin, deleteAdmin} = require('../controllers/admin.controller');
 
 router.get("/all", getAdmins);
 router.post("/register", addAdmin);
+router.put("/update/:id", updateAdmin);
+router.delete("/delete/:id", deleteAdmin);
 
-module.exports = router 
+module.exports = router;
