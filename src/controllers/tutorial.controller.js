babfdc1b964ea439030baedeeefbef88aaf620d9
“[Navodhya <navodhya2011@gmail.com>
2022-03-22 16:27:12 +0530

    Implement update, delete, GrtOne

--- src/controllers/tutorial.controller.js
+++ src/controllers/tutorial.controller.js
@@ -1,5 +1,3 @@
-const req = require("express/lib/request");
-const res = require("express/lib/response");
 const Tutorial = require("../model/TutorialModels");
 const { tutorialValidation } = require("../validations/tutorialValidation");
 
