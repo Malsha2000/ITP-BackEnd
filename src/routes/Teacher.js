c2dd41b622e51b8ae27b923232a1a9101e4d0e6b
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-03-22 23:29:07 +0530

    Update Teacher.js

--- src/routes/Teacher.js
+++ src/routes/Teacher.js
@@ -1,36 +1,9 @@
-const router = require("express").Router();
-const{ validate , Teacher} = require("../modal/TeacherModel");
-const bcrypt =require("bcrypt");
+const router = require('express').Router();
+const {addTeacher, getTeachers, updateTeacher,deleteTeacher} = require('../controllers/teacher.controller');
 
-router.post("/", async (req,res) => {
-try {
-   // const  {error} = validate( req.body);
-    if(error){
-        return res.status(400).send(
-        {
-            message: error.details[0].message
-        }
-        )};
-
-        const teacher = await Teacher.findOne({
-            email: req.body.email
-        });
-        if(teacher){
-            return res.status(409).send({message: "User with given  email altredy exist!"});
-        }
-        const salt = await bcrypt.genSalt( Number( process.env.SALT));
-        consthashPassword = await bcrypt.hash(req.body.password,salt);
-
-        await new Teacher({...req.body,password:hashPassword}).save();
-        res.status(201).send({message: "User Create Successful"});
-
-}
-catch(error){
-    res.status(500).send({message: "Internal server error"});
-}
-
-});
+router.get("/all", getTeachers);
+router.post("/register", addTeacher);
+router.put("/update/:id", updateTeacher);
+router.delete("/delete/:id", deleteTeacher);
 
 module.exports = router;
-
-
